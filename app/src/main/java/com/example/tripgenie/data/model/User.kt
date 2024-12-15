62d9a03b4240d6e116b1ddf2ea3755a26bd63de1
changminsuk <changmin6928@naver.com>
2024-12-15 14:23:02 +0900

    ✨ Feat: set User collection when login
    connect with firebase user collection

--- app/src/main/java/com/example/tripgenie/data/model/User.kt
+++ app/src/main/java/com/example/tripgenie/data/model/User.kt
@@ -1,4 +1,83 @@
 package com.example.tripgenie.data.model
 
-class User {
+data class User(
+    val uid: String = "",  // Google Auth의 UID를 저장
+    val email: String = "", // Google 계정 이메일
+    var basicInfo: BasicInfo = BasicInfo(),
+    var travelPreferences: TravelPreferences = TravelPreferences(),
+    var bookmarkedCities: MutableList<String> = mutableListOf()
+)
+
+data class BasicInfo(
+    var name: String = "-",
+    var age: Int = 0,
+    var gender: Gender = Gender.UNDISCLOSED,
+    var groupSize: Int = 1,
+)
+
+data class TravelPreferences(
+    var travelPurpose: TravelPurpose = TravelPurpose.NONE,
+    var preferredEnvironment: PreferredEnvironment = PreferredEnvironment.NONE,
+    var preferredActivities: ActivityType = ActivityType.NONE,
+    var hobbies: Hobby = Hobby.NONE,
+)
+
+enum class Gender {
+    MALE,
+    FEMALE,
+    OTHER,
+    UNDISCLOSED,
+}
+
+enum class TravelPurpose {
+    NONE,
+    LEISURE,
+    BUSINESS,
+    EDUCATION,
+    OTHER,
+}
+
+enum class PreferredEnvironment {
+    NONE,
+    URBAN,
+    RURAL,
+    MOUNTAIN,
+    BEACH,
+    DESERT,
+    OTHER,
+}
+
+enum class ActivityType {
+    NONE,
+    SIGHTSEEING,
+    SHOPPING,
+    FOOD,
+    NIGHTLIFE,
+    NATURE,
+    ADVENTURE,
+    CULTURE,
+    RELIGION,
+    HISTORY,
+    SPORTS,
+    MUSIC,
+    ART,
+    READING,
+    WRITING,
+    PHOTOGRAPHY,
+    COOKING,
+    TRAVELING,
+    OTHER,
+}
+
+enum class Hobby {
+    NONE,
+    SPORTS,
+    MUSIC,
+    ART,
+    READING,
+    WRITING,
+    PHOTOGRAPHY,
+    COOKING,
+    TRAVELING,
+    OTHER,
 }
